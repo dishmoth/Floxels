661a3a5008727fcaaa27a2b2986b0c5dd191b913
dishmoth <dishmoth@yahoo.co.uk>
2016-02-28 13:51:06 +0000

    Lock screen orientation at launch for Android.

--- android/src/com/dishmoth/floxels/android/AndroidLauncher.java
+++ android/src/com/dishmoth/floxels/android/AndroidLauncher.java
@@ -1,5 +1,7 @@
 package com.dishmoth.floxels.android;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -7,10 +9,23 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.dishmoth.floxels.FloxelsGame;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
+	
+  @Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		lockScreenOrientation();
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useWakelock = true;
 		initialize(new FloxelsGame(), config);
 	}
+  
+  private void lockScreenOrientation() {
+    int orientation = getResources().getConfiguration().orientation;
+    if ( orientation == Configuration.ORIENTATION_LANDSCAPE ) {
+      setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    } else {
+      setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+  }
+  
 }
